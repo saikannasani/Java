a9a9438e1c44dbb50b489f05ceb3048645a98dbe
saikannasani <saikannsani@gmail.com>
2025-10-18 15:38:30 +0530

    Add workflow to run HelloWorld.java automatically

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Welcome to my World, World!"); // Prints "Hello, World!" to the console.
+        //System.out.println("Welcome to my World, World!"); // Prints "Hello, World!" to the console.
+        System.out.println("Welcome to my World, World!"); // Prints message to console
     }
 }
